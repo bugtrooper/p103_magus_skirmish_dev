f7a57e3d307bb6ec70a6dbe68fc6eeafab6eadbe
bugtrooper <bugtrooper@gmail.com>
2020-04-08 15:06:39 +0200

    sr

--- engine.php
+++ engine.php
@@ -21,11 +21,17 @@ if(isset($_POST['command']))
 
 //nem stimmel a query
 
-  $query = "SELECT 'pos' FROM 'map3_units' WHERE id = '$com_who'";
+  $query = "SELECT pos FROM map3_units WHERE id = '$com_who'";
   echo "\n";
-  $com_from=mysqli_query($conn1, $query);
-  //$com_from="2004"; //ideiglenes toldás
-  echo "found where is $com_who he is at: $com_from";
+  $result=mysqli_query($conn1, $query);
+  while ($row = $result->fetch_row())
+    {
+      $com_from=$row[0];
+    }
+  echo "found where is ";
+  echo $com_who;
+  echo " he is at: ";
+  echo $com_from;
   echo "\n";
   echo "\n";
   echo "\n";
@@ -36,17 +42,25 @@ if(isset($_POST['command']))
     //move
     //original unitpos erase
     //to pos update
-    $query = "SELECT 'kep' FROM 'map3_units' WHERE id = '$com_who'";
-    $unit_pic=mysqli_query($conn1, $query);
-    echo "'unitpic='+'$unit_pic'";
-    $query = "UPDATE 'map3_units' SET pos= '$com_to'  WHERE id = '$com_who'";
+    //$query = "SELECT 'kep' FROM 'map3_units' WHERE id = '$com_who'";
+  //  $unit_pic=mysqli_query($conn1, $query);
+  //  echo "'unitpic='+'$unit_pic'";
+  $query = "SELECT kep FROM map3_units WHERE id = '$com_who'";
+  $result=mysqli_query($conn1, $query);
+  while ($row = $result->fetch_row())
+    {
+      $com_pic=$row[0];
+    }
+
+
+    $query = "UPDATE map3_units SET pos= '$com_to'  WHERE id = '$com_who'";
     mysqli_query($conn1, $query);
-    echo "updated map3_units positionof:"+$com_who;
-    $query = "UPDATE 'map3' SET unitid= '0'  WHERE id = '$com_from'";
+    //echo "updated map3_units positionof:"+$com_who;
+    $query = "UPDATE map3 SET unitid= '0'  WHERE id = '$com_from'";
     mysqli_query($conn1, $query);
-    $query = "UPDATE 'map3' SET unitid= '1'  WHERE id = '$com_to'";
+    $query = "UPDATE map3 SET unitid= '$com_pic'  WHERE id = '$com_to'";
     mysqli_query($conn1, $query);
-    echo "moved unit from:"+$com_from+" to:"+$com_to;
+    //echo "moved unit from:"+$com_from+" to:"+$com_to;
   }
   if($com_what==2)
   {
