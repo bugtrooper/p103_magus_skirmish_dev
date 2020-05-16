fcd4b6b742ddf4b70dcb08bf09ae6440cf91ede2
BuildTools <unconfigured@null.spigotmc.org>
2020-05-16 15:04:17 +0200

    CSS grid

--- index.php
+++ index.php
@@ -1,37 +1,43 @@
 <head>
 	<title>Magus Skirmish Project</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
-	<div id="Headline">
+<div class="site">
+	<div class="green" id="Headline">
 		<p>Kitöltés</p>
-		<div id="Login">
-		<?php
-			session_start();
-			if (isset($_SESSION['userId'])) {
-				echo '<form action="processing/logout.php" method="post">
-					<button type="submit">Logout</button>
-				</form>';
-			}
-			else {
-				echo '<form action="processing/login.php" method="post">
-					<input type="text" name="mailuid" placeholder="Username/E-mail...">
-					<input type="password" name="pwd" placeholder="Password">
-					<button type="submit" name="login-submit">Login</button>
-				</form>
-				<a href="signup.php">Signup</a>';
+	</div>
+	<div id="Login">
+	<?php
+		session_start();
+		if (isset($_SESSION['userId'])) {
+			echo '<form action="processing/logout.php" method="post">
+				<button type="submit">Logout</button>
+			</form>';
+		}
+		else {
+			echo '
+			<div class="login-cred">
+				<form action="processing/login.php" method="post">
+				<input type="text" name="mailuid" placeholder="Username/E-mail...">
+				<input type="password" name="pwd" placeholder="Password">
+			</div>
+			<div class="login-buttons">
+				<button type="submit" name="login-submit">Login</button>
+			  <a href="signup.php">Signup</a>
+			</div>
+			</form>'
+			;
 
-			}
-		?>
-		</div>
+		}
+	?>
 	</div>
 
 
 
-<div id="body">
-	<div id="game">
+<!-- <div id="body"> -->
+	<div class="red" id="game">
 		<?php
 		require_once 'processing/Adatbazis.php';
 		require_once 'processing/engine.php';
@@ -54,7 +60,7 @@
 			echo "</table><br>"	;
 		?>
 	</div>
-	<div id="gombok">
+	<div class="blue" id="gombok">
 		<button type="button" onclick=send()>previous</button>
 		<button type="button" onclick=reiceve()>next</button>
 		<button type="button" onclick=move_action()>move</button>
@@ -62,5 +68,6 @@
 		<button type="button" onclick=refresh()>new</button>
 		<script src="core.js" defer></script>
 	</div>
-	<div style="position: absolute; z-index: 1; margin: 4px; pointer-events: none;">M.A.G.U.S. skirmish ALPHA 001</div>
+	<div class="purple" id="gamephase">M.A.G.U.S. skirmish ALPHA 001</div>
+</div>
 </body>
